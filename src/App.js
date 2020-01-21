b8115950c3e2d81853e21b90e6c57ea8e0742b79
Saad Azam <saad.azam@magna.com>
2020-01-21 18:55:55 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ocean is Blue!
+          Ocean is Blue and there is a newly created feature branch!
         </p>
         <a
           className="App-link"
